28a3ff65dd2e7475512dddbf96394f26b15b202b
Pierre-Jean CROUZET <pjcrouzet.externe@bedrockstreaming.com>
2025-11-21 16:26:44 +0100

    feat: add PriorityClassName to pod kube_types
    Signed-off-by: Pierre-Jean CROUZET <pjcrouzet.externe@bedrockstreaming.com>

--- pkg/deployer/deployer_test.go
+++ pkg/deployer/deployer_test.go
@@ -327,6 +327,7 @@ var _ = Describe("Deployer", func() {
 							},
 						},
 						PodTemplate: &gw2_v1alpha1.Pod{
+							PriorityClassName: ptr.To("default"),
 							TopologySpreadConstraints: []corev1.TopologySpreadConstraint{
 								{
 									MaxSkew:           1,
@@ -627,6 +628,7 @@ var _ = Describe("Deployer", func() {
 			Expect(deployment.Spec.Template.Spec.TopologySpreadConstraints[0]).To(Equal(gwp.Spec.Kube.PodTemplate.TopologySpreadConstraints[0]))
 			Expect(deployment.Spec.Template.Spec.Tolerations[0]).To(Equal(gwp.Spec.Kube.PodTemplate.Tolerations[0]))
 			Expect(deployment.Spec.Template.Spec.NodeSelector).To(Equal(gwp.Spec.Kube.PodTemplate.NodeSelector))
+			Expect(*deployment.Spec.Template.Spec.PriorityClassName).To(Equal(gwp.Spec.Kube.PodTemplate.PriorityClassName))
 			// check env values are appended to the end of the list
 			var testEnvVar corev1.EnvVar
 			for _, envVar := range deployment.Spec.Template.Spec.Containers[0].Env {
